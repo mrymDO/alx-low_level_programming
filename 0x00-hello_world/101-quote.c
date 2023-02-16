53a7d3dbca28620db2f7a613bf8b1ea3fc90f8f8
Mrym Dobli <dobli.meryeme@gmail.com>
2023-02-16 11:02:15 -0800

    prints a line to the standars error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a program that prints a line to the error standard
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
