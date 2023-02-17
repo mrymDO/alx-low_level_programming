036ec340baf9008a091a9afd7f6bc152d0873bca
Mrym Dobli <dobli.meryeme@gmail.com>
2023-02-17 11:55:46 -0800

    Combinations of single digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,17 +6,18 @@
  */
 int main(void)
 {
-	int a
+	int num;
 
-	for (a = 0; a < 1; a++)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar(a +'0');
-		if (a < 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
 	}
+
 	putchar('\n');
 	return (0);
 }
