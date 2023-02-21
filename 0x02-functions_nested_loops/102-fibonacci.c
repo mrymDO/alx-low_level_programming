74cd4a1d8f93962a7f203466ec1a8a63e6b4ba9d
Mrym Dobli <dobli.meryeme@gmail.com>
2023-02-21 15:28:53 -0800

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -  the first 50 Fibonacci numbers starting with 1 and 2
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int i, j, c, count;
+
+	i = 0;
+	j = 0;
+	for (count = 0; count < 50; count++)
+	{
+		c = i + j;
+		i = j;
+		j = c;
+		printf("%ld", c);
+		if (count == 49)
+			putchar('\n');
+		else
+			putchar(', ');
+	}
+}
