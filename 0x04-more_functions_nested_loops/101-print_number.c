073879d07581cc1785ecf99d89ea6ece3aee85cf
Mrym Dobli <dobli.meryeme@gmail.com>
2023-02-23 13:06:18 -0800

    print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - Print an integer
+ * @n: the integer to print
+ */
+
+void print_number(int n)
+{
+	unsigned int m = n;
+
+	if (n < 0)
+	{
+		n *= -n;
+		m = n;
+		_putchar('-');
+	}
+	m /= 10;
+	if (m != 0)
+	{
+		print_number(m);
+	}
+
+	_putchar((unsigned int) n % 10 + '0');
+}
+
