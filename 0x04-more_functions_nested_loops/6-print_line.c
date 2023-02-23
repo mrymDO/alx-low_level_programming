a2351c65e9a7b6a5b71d0197d1236b60fcd60faf
Mrym Dobli <dobli.meryeme@gmail.com>
2023-02-23 06:32:11 -0800

    straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,18 +6,18 @@
  */
 void print_line(int n)
 {
+	int i;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-	int i;
-
-	for (i = 0; i <= n; i++)
-	{
-		_putchar('_');
-	}
-	_putchar('\n');
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
 }
