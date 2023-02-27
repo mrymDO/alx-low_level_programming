fde788716b1a1ade378f0126415a060521932833
Mrym Dobli <dobli.meryeme@gmail.com>
2023-02-27 15:27:31 -0800

    10- string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to be converted
+ */
+int _atoi(char *s)
+{
+	int i, j, k;
+
+	j = 0;
+	k = -1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	if (s[i] == '-')
+	k *= -1;
+
+	if (s[i] > 47 && s[i] < 58)
+	{
+	if (j < 0)
+	j = (j * 10) - (s[i] - '0');
+	else
+	j = (s[i] - '0') * -1;
+
+	if (s[i + 1] <= 47 || s[i + 1] >= 58)
+		break;
+	}
+	}
+	if (k < 0)
+		j *= -1;
+
+	return (j);
+}
