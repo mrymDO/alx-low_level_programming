96f13abaaafbc91bb83a35ce44441d906a284136
Mrym Dobli <dobli.meryeme@gmail.com>
2023-03-01 11:18:57 -0800

    3.fix strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; s1[i]; i++)
+	for (i = 0; s1[i] && s2[i]; i++)
 	{
-		for (j = 0; s2[j]; j++)
-		{
-			if (s1[i] != s2[j])
+		if (s1[i] != s2[i])
 			return (*s1 - *s2);
-		}
 	}
 	return (0);
 }
