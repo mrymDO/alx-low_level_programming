b5ca3587a1734a5dc982798c8f5cb1203fd28f75
Mrym Dobli <dobli.meryeme@gmail.com>
2023-03-01 04:58:27 -0800

    3.strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - A function that compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 0 (success)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	for (i = 0; s1[i]; i++)
+	{
+		for (j = 0; s2[j]; j++)
+		{
+			return (*s1 - *s2);
+		}
+	}
+	return (0);
+}
