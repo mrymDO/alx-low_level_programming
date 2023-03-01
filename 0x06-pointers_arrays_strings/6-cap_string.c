ed3044c13ad2f78430b185067df540a172a9113d
Mrym Dobli <dobli.meryeme@gmail.com>
2023-03-01 13:59:16 -0800

    6.fix cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,12 +14,14 @@ char *cap_string(char *str)
 	i = 0;
 	while (str[i])
 	{
-	while (!(str[i] >= 'a' && str[i] <= 'z'))
-
-		i++;
 	if (i == 0 || str[i - 1] == *separators)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
 		str[i] -= 32;
-
+		}
+	}
+	i++;
 	}
 	return (str);
 }
