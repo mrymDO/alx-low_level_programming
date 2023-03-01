7e71df940fcec7ca4ab615faf4ca39820cae366e
Mrym Dobli <dobli.meryeme@gmail.com>
2023-03-01 14:34:33 -0800

    6.Cap String

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,17 +11,27 @@ char *cap_string(char *str)
 	int i;
 	int separators[] = {',', ';', '.', '?', '"',
 			    '(', ')', '{', '}', ' ', '\n', '\t', '!'};
+
 	i = 0;
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] -= 32;
+	}
+
+	i++;
 	while (str[i])
 	{
-	if (i == 0 || str[i - 1] == *separators)
-	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		int j;
+
+		for (j = 0; j < 13; j++)
 		{
-		str[i] -= 32;
+		if (str[i] == separators[j])
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			str[i + 1] -= 32;
 		}
-	}
-	i++;
+		}
+		i++;
 	}
 	return (str);
 }
