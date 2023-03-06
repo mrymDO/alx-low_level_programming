a2dd2bc527aa5a66aae337a4523127843a1c1c69
Mrym Dobli <dobli.meryeme@gmail.com>
2023-03-06 10:31:18 -0800

    5.strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h>"
+/**
+ * _strstr - locates a substring
+ * @haystack: string to be scanned
+ * @needle: small string to be searched
+ * Return: NULL if no match. Else, pointer to first occurence
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+	for (; haystack[i]; haystack++)
+	{
+		char *a = haystack;
+		char *b = needle;
+
+		while (*a == *b && *b != '\0)
+		{
+			a++;
+			b++;
+		}
+			if(*b == '\0')
+				return (haystack);
+	}
+	return(NULL);
+}
