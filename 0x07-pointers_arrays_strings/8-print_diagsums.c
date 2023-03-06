5760b40d90308e2fbbb1c10d4b66e3f14b90fcf2
Mrym Dobli <dobli.meryeme@gmail.com>
2023-03-06 13:48:28 -0800

    8_diagosums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,10 +10,9 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, sum1, sum2;
-
-	sum1 = 0;
-	sum2 = 0;
+	int i;
+	int sum1 = 0;
+	int sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
@@ -21,7 +20,7 @@ void print_diagsums(int *a, int size)
 	}
 	for (i = size - 1; i >= 0; i--)
 	{
-		sum2 += sum2 + a[i * size + (size - i - 1)];
+		sum2 += a[i * size + (size - i - 1)];
 	}
 
 	printf("%d, %d\n", sum1, sum2);
