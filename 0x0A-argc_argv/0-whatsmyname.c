f4d3dc082a271d4f2a79b63484eca1d5a4ead7ce
Meryem Dobli <dobli.meryeme@gmail.com>
2023-03-10 06:50:32 -0800

    0.program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
