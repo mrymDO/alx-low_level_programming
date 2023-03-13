485240d96f819043fa28ae7e345641c448081f7f
Meryem Dobli <dobli.meryeme@gmail.com>
2023-03-13 05:53:25 -0700

    3.alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	array = malloc(height *  sizeof(int));
+	array = malloc(height *  sizeof(int *));
 
 	if (array == NULL)
 		return (NULL);
