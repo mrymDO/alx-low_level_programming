a8fd966836edd9f4430adc63d4a097566449ce48
Meryem Dobli <dobli.meryeme@gmail.com>
2023-03-15 07:42:23 -0700

    6.split_words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,17 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * free_words - frees memory
+ * @string: pointer to string to be free
+ * @i: counter
+ */
+void free_words(char **string, int i)
+{
+	for (; i > 0;)
+		free(string[--i]);
+	free(string);
+}
+/**
  * strtow - function that splits a string into words
  * @str: input string
  * Return: a pointer to an array of strings or NULL if it fails
@@ -12,17 +23,14 @@ char **strtow(char *str)
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-
 	num = 0;
 	for (i = 0; str[i]; i++)
-	{
 		if (str[i] != ' ' && (str[i] == 0 || str[i - 1] == ' '))
 			num++;
-	}
+
 	words = malloc((num + 1) * sizeof(char *));
 	if (words == NULL)
 		return (NULL);
-
 	j = 0;
 	for (i = 0; str[i]; i++)
 	{
@@ -30,10 +38,13 @@ char **strtow(char *str)
 		{
 			k = i;
 			while (str[k] != ' ' && str[k] != '\0')
-			{
 				k++;
-			}
 			words[j] = malloc((k - i + 1) * sizeof(char));
+			if (words[j] == 0)
+			{
+				free_words(words, j);
+				return (NULL);
+			}
 			k = 0;
 			while (str[i] != ' ' && str[i] != '\0')
 			{
