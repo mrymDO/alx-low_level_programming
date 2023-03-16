d5f9db566ab90c558d572b3852988be3870201ab
Meryem Dobli <dobli.meryeme@gmail.com>
2023-03-16 03:21:33 -0700

    fix task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,15 +11,22 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *a;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
+	{
 		return (NULL);
+	}
 
 	a = malloc(nmemb * size);
-	if (a == NULL)
-		return (NULL);
 
-	*a = 0;
+	if (a != NULL)
+	{
+		for (i = 0; i < nmemb; i++)
+		{
+			a[i] = 0;
+		}
+	}
 
 	return (a);
 }
