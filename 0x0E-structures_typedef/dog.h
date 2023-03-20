7e1b84d32711d644d9731f432b082053c0f59eac
Meryem Dobli <dobli.meryeme@gmail.com>
2023-03-20 07:27:11 -0700

    1.struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's info
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+
+#endif
