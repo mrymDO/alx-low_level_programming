32ff281f727a68d6b637ebba089fd1a7cdaf7373
Meryem Dobli <dobli.meryeme@gmail.com>
2023-03-22 14:17:25 -0700

    3.Arithmetic op fix betty

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,16 +24,17 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 
 	arith_op = get_op_func(argv[2]);
+
 	if (arith_op == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%'))
+	if (num2 == 0 && (*argv[2] == '%' || *argv[2] == '/'))
 	{
-	       printf("Error\n");
-	       exit(100);
+		printf("Error\n");
+		exit(100);
 	}
 
 	printf("%d\n", arith_op(num1, num2));
