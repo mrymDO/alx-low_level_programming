f4edf5b19976affad27a9191faca289838e801ec
Mrym <dobli.meryeme@gmail.com>
2023-03-30 07:08:46 -0700

    1.number of nodes

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list list_t
+ * @h: a pointer to the head node of the linked list
+ * Return: the length of the list as a value of type size_t
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+	if (h->str == NULL)
+		return (1);
+
+	count++;
+	h = h->next;
+	}
+
+	return (count);
+}
+
