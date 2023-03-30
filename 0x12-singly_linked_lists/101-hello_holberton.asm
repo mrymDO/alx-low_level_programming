cedad698683162d8852a527e80d69299f4890ed7
Mrym <dobli.meryeme@gmail.com>
2023-03-30 14:29:56 -0700

    assembly code

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+section .data
+    hello db 'Hello, Holberton',0
+    format db '%s\n',0
+
+section .text
+    global main
+    extern printf
+
+main:
+    push rbp
+    mov rbp, rsp
+
+    push qword hello
+    push qword format
+    call printf
+    add rsp, 16
+
+    mov rsp, rbp
+    pop rbp
+    ret
+
