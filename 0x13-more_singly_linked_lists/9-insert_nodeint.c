1bb4f36b7943c08d983187ce609d6086620ac58b
Mrym <dobli.meryeme@gmail.com>
2023-04-03 14:21:03 -0700

    fix task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,6 +36,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 		i++;
 	}
+
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 		new_node->next = current->next;
 		current->next = new_node;
 
