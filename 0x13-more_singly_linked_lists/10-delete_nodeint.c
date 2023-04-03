7f1327fdf861f95d621cf9ee88067d8e6f686eee
Mrym <dobli.meryeme@gmail.com>
2023-04-03 14:44:03 -0700

    fix task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -33,7 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		i++;
 	}
 
-	if (current->next == NULL)
+	if (current == NULL || current->next == NULL)
 		return (-1);
 
 	deletenode = current->next;
