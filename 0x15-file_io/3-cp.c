c254667a6d68ea22589076792dc89eab512b3e47
Mrym <dobli.meryeme@gmail.com>
2023-04-09 15:44:36 -0700

    fix copyt content file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,25 +12,38 @@
  */
 int main(int argc, char **argv)
 {
-	int fd_to, fd_from, close_check;
-	char buffer[SIZE];
-	ssize_t n_read, n_written;
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-	fd_from = open(argv[1], O_RDONLY);
+	copy_file(argv[1], argv[2]);
+
+	return (0);
+}
+/**
+ * copy_file - copies the content of a file to another file
+ * @file_from: the file to copy from
+ * @file_to: the file destination
+ * Return: 0 on success
+ */
+int copy_file(char *file_from, char *file_to)
+{
+	int fd_to, fd_from, close_check;
+	char buffer[SIZE];
+	ssize_t n_read, n_written;
+
+	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", file_from);
 		exit(98);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error, Can't write to file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error, Can't write to file %s\n", file_to);
+		close(fd_from);
 		exit(99);
 	}
 	while ((n_read = read(fd_from, buffer, SIZE)) > 0)
@@ -38,15 +51,15 @@ int main(int argc, char **argv)
 		n_written = write(fd_to, buffer, n_read);
 		if (n_written == -1 || n_written != n_read)
 		{
-			dprintf(STDERR_FILENO, "Error, Can't write to file %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error, Can't write to file %s\n", file_to);
 			close(fd_from);
 			close(fd_to);
 			exit(99);
 		}
-	}		
+	}
 	if (n_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", file_from);
 		close(fd_from);
 		close(fd_to);
 		exit(98);
