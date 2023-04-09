c7d0f4141688dd0ce1c54b920b013160810c5ec7
Mrym <dobli.meryeme@gmail.com>
2023-04-09 16:19:56 -0700

    copy file to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
  */
 int copy_file(char *file_from, char *file_to)
 {
-	int fd_to, fd_from, close_check;
+	int fd_to, fd_from;
 	char buffer[SIZE];
 	ssize_t n_read, n_written;
 
@@ -43,7 +43,7 @@ int copy_file(char *file_from, char *file_to)
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error, Can't write to file %s\n", file_to);
-		close(fd_from);
+		close_file(fd_from, file_from);
 		exit(99);
 	}
 	while ((n_read = read(fd_from, buffer, SIZE)) > 0)
@@ -52,28 +52,37 @@ int copy_file(char *file_from, char *file_to)
 		if (n_written == -1 || n_written != n_read)
 		{
 			dprintf(STDERR_FILENO, "Error, Can't write to file %s\n", file_to);
-			close(fd_from);
-			close(fd_to);
+			close_file(fd_from, file_from);
+			close_file(fd_to, file_to);
 			exit(99);
 		}
 	}
 	if (n_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", file_from);
-		close(fd_from);
-		close(fd_to);
+		close_file(fd_from, file_from);
+		close_file(fd_to, file_to);
 		exit(98);
 	}
-	close_check = close(fd_from);
+
+	close_file(fd_from, file_from);
+	close_file(fd_to, file_to);
+	return (0);
+}
+/**
+ * close_file - close file descriptor and checks for error
+ * @fd: file descriptor
+ * @filename: pointer to char string represent name of the file
+ * Return: 0 on success
+ */
+int close_file(int fd, char *filename)
+{
+	int close_check;
+
+	close_check = close(fd);
 	if (close_check == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	close_check = close(fd_to);
-	if (close_check == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd_to);
+		dprintf(STDERR_FILENO, "Error: can't close fd for file %s\n", filename);
 		exit(100);
 	}
 	return (0);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,5 +9,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int copy_file(char *file_from, char *file_to);
+int close_file(int fd, char *filename);
 
 #endif
