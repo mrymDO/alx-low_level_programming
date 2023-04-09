90eafaaadc552724e123bcda6a28df666a460397
Mrym <dobli.meryeme@gmail.com>
2023-04-09 14:42:23 -0700

    cp file to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,7 +21,6 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
@@ -40,15 +39,18 @@ int main(int argc, char **argv)
 		if (n_written == -1 || n_written != n_read)
 		{
 			dprintf(STDERR_FILENO, "Error, Can't write to file %s\n", argv[2]);
+			close(fd_from);
+			close(fd_to);
 			exit(99);
 		}
 	}		
 	if (n_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+		close(fd_from);
+		close(fd_to);
 		exit(98);
 	}
-
 	close_check = close(fd_from);
 	if (close_check == -1)
 	{
