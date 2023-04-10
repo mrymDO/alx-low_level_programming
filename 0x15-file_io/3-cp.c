c40ac0cec4f64f7d915f345ae402a6440e540a3b
Mrym <dobli.meryeme@gmail.com>
2023-04-10 03:32:54 -0700

    copy from to

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,9 +25,9 @@ int main(int argc, char **argv)
  * copy_file - copies the content of a file to another file
  * @file_from: the file to copy from
  * @file_to: the file destination
- * Return: 0 on success
+ * Return: void
  */
-int copy_file(char *file_from, char *file_to)
+void copy_file(char *file_from, char *file_to)
 {
 	int fd_to, fd_from;
 	char buffer[SIZE];
@@ -39,11 +39,10 @@ int copy_file(char *file_from, char *file_to)
 		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", file_from);
 		exit(98);
 	}
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error, Can't write to file %s\n", file_to);
-		close_file(fd_from, file_from);
 		exit(99);
 	}
 	while ((n_read = read(fd_from, buffer, SIZE)) > 0)
@@ -52,38 +51,31 @@ int copy_file(char *file_from, char *file_to)
 		if (n_written == -1 || n_written != n_read)
 		{
 			dprintf(STDERR_FILENO, "Error, Can't write to file %s\n", file_to);
-			close_file(fd_from, file_from);
-			close_file(fd_to, file_to);
 			exit(99);
 		}
 	}
 	if (n_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", file_from);
-		close_file(fd_from, file_from);
-		close_file(fd_to, file_to);
 		exit(98);
 	}
 
-	close_file(fd_from, file_from);
-	close_file(fd_to, file_to);
-	return (0);
+	close_file(fd_from);
+	close_file(fd_to);
 }
 /**
  * close_file - close file descriptor and checks for error
  * @fd: file descriptor
- * @filename: pointer to char string represent name of the file
- * Return: 0 on success
+ * Return: void
  */
-int close_file(int fd, char *filename)
+void close_file(int fd)
 {
 	int close_check;
 
 	close_check = close(fd);
 	if (close_check == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't close fd for file %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd);
 		exit(100);
 	}
-	return (0);
 }
