95063c141488cec63c521c9fb892db9f1b879457
Mrym <dobli.meryeme@gmail.com>
2023-04-10 14:01:15 -0700

    3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,60 +12,46 @@
  */
 int main(int argc, char **argv)
 {
+	int fd_to, fd_from;
+	char buffer[SIZE];
+	ssize_t n_read;
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	copy_file(argv[1], argv[2]);
-
-	return (0);
-}
-/**
- * copy_file - copies the content of a file to another file
- * @file_from: the file to copy from
- * @file_to: the file destination
- * Return: void
- */
-void copy_file(char *file_from, char *file_to)
-{
-	int fd_to, fd_from;
-	char buffer[SIZE];
-	ssize_t n_read;
-
-	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR |
-			S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-
-	fd_from = open(file_from, O_RDONLY);
-
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error, Can't write to file %s\n", file_to);
-		exit(99);
-	}
-
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, S_IRUSR |
+			S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error, Can't write to file %s\n", argv[2]);
+		exit(99);
+	}
 	while ((n_read = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
 		if (write(fd_to, buffer, n_read) != n_read)
 		{
-			dprintf(STDERR_FILENO, "Error, Can't write to file %s\n", file_to);
+			close_file(fd_to);
+			close_file(fd_from);
+			dprintf(STDERR_FILENO, "Error, Can't write to file %s\n", argv[2]);
 			exit(99);
 		}
 	}
-
 	if (n_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	close_file(fd_to);
 	close_file(fd_from);
+	return (0);
 }
 /**
  * close_file - close file descriptor and checks for error
