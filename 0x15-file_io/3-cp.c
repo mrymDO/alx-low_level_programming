76e922b3531ad727351b8b7021a57ab46aca6d00
Mrym <dobli.meryeme@gmail.com>
2023-04-10 16:23:18 -0700

    FIX

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error, Can't write to %s\n", argv[2]);
@@ -42,7 +42,7 @@ int main(int argc, char **argv)
 	while (n_read > 0)
 	{
 		n_written = write(fd_to, buffer, n_read);
-		if (n_written == -1 || n_written != n_read)
+		if (n_written == -1)
 		{
 			dprintf(STDERR_FILENO, "Error, Can't write to %s\n", argv[2]);
 			exit(99);
