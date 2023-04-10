08ba59d0b31ac23189290efe4b3dd1a368762e70
Mrym <dobli.meryeme@gmail.com>
2023-04-10 15:15:13 -0700

    fix betty task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#define SIZE 1024
 /**
  * main - a program that copies the content of a file to another file
  * @argc: number of argument
@@ -13,38 +13,41 @@
 int main(int argc, char **argv)
 {
 	int fd_to, fd_from;
-	char buffer[1024];
-	ssize_t n_read;
+	ssize_t n_read, n_written;
+	char buffer[SIZE];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]), exit(98);
 	}
-	fd_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR |
-			S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error, Can't write to %s\n", argv[2]);
+		close_file(fd_from);
 		exit(99);
 	}
-	while ((n_read = read(fd_from, buffer, sizeof(buffer))) > 0)
+	while ((n_read = read(fd_from, buffer, SIZE)) > 0)
 	{
-		if (write(fd_to, buffer, n_read) != n_read)
+		n_written = write(fd_to, buffer, n_read);
+		if (n_written == -1 || n_written != n_read)
 		{
 			dprintf(STDERR_FILENO, "Error, Can't write to %s\n", argv[2]);
+			close_file(fd_to);
+			close_file(fd_from);
 			exit(99);
 		}
 	}
 	if (n_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+		close_file(fd_to);
+		close_file(fd_from);
 		exit(98);
 	}
 	close_file(fd_to);
