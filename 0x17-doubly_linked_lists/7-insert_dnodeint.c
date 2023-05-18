09cd31aa126145a592cbd5814f489efaaafe581e
Meryem <dobli.meryeme@gmail.com>
2023-05-18 08:11:07 -0700

    fix2 insert a node at a specific position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,6 +13,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *current;
 	unsigned int i = 0;
 
+	 if (idx == 0)
+                return (add_dnodeint(&new_node, n));
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -20,9 +23,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
-
-	if (idx == 0)
-		return (add_dnodeint(&new_node, n));
 
 	current = *h;
 
@@ -36,6 +36,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint_end(&new_node, n));
 
 	new_node->next =  current->next;
+	if (current->next != NULL)
+		current->next->prev = new_node;
 	current->next = new_node;
 	new_node->prev = current;
 
