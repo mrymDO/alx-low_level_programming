6480fcef0bb48559690e1bbb95e3e0356912d651
Meryem <dobli.meryeme@gmail.com>
2023-05-18 04:41:52 -0700

    fix3 add a new node at the begining of a double linked lis

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,12 +16,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->next = NULL;
 	new_node->prev = NULL;
 
 	if (*head == NULL)
 	{
-		*head = new_node;
+		new_node->next = *head;
 	}
 	else
 	{
