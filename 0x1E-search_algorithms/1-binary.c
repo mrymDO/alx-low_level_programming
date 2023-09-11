ee8a4f78446de3690641f744233686cd06b32767
Meryem <dobli.meryeme@gmail.com>
2023-09-11 14:14:32 -0700

    print array as the example

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,23 +10,21 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	int l, r, mid, i;
+	size_t l = 0, r = size - 1, mid = 0, i;
 
-	if (array == NULL || size == 0)
+	if (array == NULL)
 		return (-1);
 
-	l = 0;
-	r = size - 1;
 
 	while (l <= r)
 	{
 		mid = l + (r - l) / 2;
 		printf("Searching in array:");
-		for (i = l; i <= r; i++)
+		for (i = l; i < r; i++)
 		{
-			printf("%d ", array[i]);
+			printf("%d, ", array[i]);
 		}
-		printf("\n");
+		printf("%d\n", array[r]);
 		if (array[mid] == value)
 			return (mid);
 		else if (value > array[mid])
